23765425fe0ef0cc56c4e04d270d63d0d77afbf3
Rob Bertram <robert.bertram@livingasone.com>
2020-09-14 21:06:42 -0500

    my todo

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,6 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[wasm_bindgen]
 pub fn run_app() -> Result<(), JsValue> {
     wasm_logger::init(wasm_logger::Config::default());
-    yew::start_app::<app::App>();
+    yew::start_app::<app::Model>();
     Ok(())
 }
